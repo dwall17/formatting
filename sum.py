82942d60aa313151e7dcfb4543ab1a06cd83f3e0
Dylan Wallace <dylanwallace17@gmail.com>
2025-02-05 18:19:27 -0500

    add a print again

--- sum.py
+++ sum.py
@@ -1,6 +1,8 @@
-def add_numbers(a,b):
-    result=a+b
+def add_numbers(a, b):
+    result = a + b
     return result
 
-print(add_numbers(4,5))
+
+print(add_numbers(4, 5))
+print(add_numbers(9, 5))
 print("this is a push test")
